6e4dc927e2815f2ae43382daa7ae41da276764fc
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-05-16 10:43:42 +0200

    Mapp och fil struktur klar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [page, setPage] = useState<string>("");
+
+  useEffect(() => {
+    let pageUrl = page;
+
+    if (!pageUrl) {
+      const queryParams = new URLSearchParams(window.location.search);
+      const getUrl = queryParams.get("page");
+
+      if (getUrl) {
+        pageUrl = getUrl;
+        setPage(getUrl);
+      } else {
+        pageUrl = "project";
+      }
+    }
+
+    window.history.pushState(null, "", "?page=" + pageUrl);
+  }, [page]);
   return (
     <>
       <h1>Hej</h1>
