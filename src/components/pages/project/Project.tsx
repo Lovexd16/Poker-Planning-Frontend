0e4f3695b980d836b81ee9cabe80d812251cd86f
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-05-21 14:41:48 +0200

    Nu står det att man inte har några projects om man inte har det

--- src/components/pages/project/Project.tsx
+++ src/components/pages/project/Project.tsx
@@ -1,42 +1,49 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { jwtDecode } from "jwt-decode";
 
 import ProjectInterface from "../../interface/ProjectInterface";
 
-
 function Project() {
-
   const [projects, setProjects] = useState<ProjectInterface[]>([]);
-  const [selectedProject, setSelectedProject] = useState<ProjectInterface | null>(null);
+  const [selectedProject, setSelectedProject] =
+    useState<ProjectInterface | null>(null);
 
-  const selectProject = (project: ProjectInterface) =>{
-    setSelectedProject(project)
-  }
+  const selectProject = (project: ProjectInterface) => {
+    setSelectedProject(project);
+  };
 
   useEffect(() => {
-    const token = localStorage.getItem('token') || '';
+    const token = localStorage.getItem("token") || "";
     const decodedToken = jwtDecode(token);
     const loggedInUser = decodedToken.sub;
 
     fetch(`http://localhost:8080/projects/${loggedInUser}`, {
       headers: {
-        Authorization: `Bearer ${token}`
-      }
+        Authorization: `Bearer ${token}`,
+      },
     })
-    .then(res => res.json())
-    .then(data => setProjects(data));
+      .then((res) => res.json())
+      .then((data) => setProjects(data));
   }, [projects]);
 
   return (
     <div>
       <h2>Projekt:</h2>
-      {projects.map((project: ProjectInterface) =>(
+      {projects.length > 0 ? (
+        projects.map((project: ProjectInterface) => (
           <div key={project.projectId}>
-          <button onClick={() => selectProject(project)}>{project.projectCreatedByUser.username + "/" + project.projectName}</button>
+            <button onClick={() => selectProject(project)}>
+              {project.projectCreatedByUser.username +
+                "/" +
+                project.projectName}
+            </button>
           </div>
-      ))}
+        ))
+      ) : (
+        <p>Du har inga aktiva projekt.</p>
+      )}
     </div>
-  )
+  );
 }
 
 export default Project;
