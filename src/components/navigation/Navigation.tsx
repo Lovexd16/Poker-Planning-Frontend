56c8dcdeceb37fe392bacd24ebfc7bfd5e65d1e9
Viktor Falkesjö <viktorfalkesjo@gmail.com>
2024-05-22 13:45:23 +0200

    ghsdghsgh

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,5 +1,4 @@
 import Logout from "../pages/login/Logout";
-import NewProject from "../pages/project/NewProject";
 
 interface Props {
   setPage: ((page: string) => void);
@@ -12,12 +11,12 @@ function Navigation({ setPage, isLoggedIn, setIsLoggedIn }: Props) {
   return (
     <div className="header">
       {isLoggedIn ? (
-        <><Logout setPage={setPage} setIsLoggedIn={setIsLoggedIn} /><button onClick={() => setPage("newproject")}>Skapa nytt projekt</button></>
+        <><Logout setPage={setPage} setIsLoggedIn={setIsLoggedIn} />
+        <button onClick={() => setPage("newproject")}>Skapa nytt projekt</button>
+        <button onClick={() => setPage("project")}>Projekt</button></>
       ) : (
-        <button onClick={() => setPage("login")}>Logga in</button>
-      )}
-      {!isLoggedIn && (
-        <button onClick={() => setPage("register")}>Registrera</button>
+        <><button onClick={() => setPage("login")}>Logga in</button>
+        <button onClick={() => setPage("register")}>Registrera</button></>
       )}
     </div>
   );
---
+++ src/components/pages/project/InviteUser.tsx
@@ -0,0 +1,13 @@
+function InviteUser ({projectId}: {projectId: string}) {
+
+   
+
+
+    return (  
+        <>
+        <h2>Bjud in</h2>
+        </>
+    );
+}
+
+export default InviteUser ;
