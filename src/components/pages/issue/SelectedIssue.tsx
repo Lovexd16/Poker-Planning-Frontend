ceeeb7bb18765b3c75952419f6576a1e8f4aa99d
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-05-24 09:49:47 +0200

    Nu kan man markera issue som klart

--- src/components/pages/issue/SelectedIssue.tsx
+++ src/components/pages/issue/SelectedIssue.tsx
@@ -1,34 +1,45 @@
 import { useEffect, useState } from "react";
 import IssueInterface from "../../interface/IssueInterface";
+import IssueIsDone from "./IssueIsDone";
 
-function SelectedIssue({issueId}: {issueId: string}) {
+function SelectedIssue({ issueId }: { issueId: string }) {
+  const [selectedIssue, setSelectedIssue] = useState<IssueInterface | null>(
+    null
+  );
 
-    const [selectedIssue, setSelectedIssue] = useState<IssueInterface | null>(null);
+  useEffect(() => {
+    fetch(`http://localhost:8080/issue/${issueId}`)
+      .then((res) => res.json())
+      .then((data) => setSelectedIssue(data))
+      .catch((error) => console.error("Error fetching project:", error));
+  }, []);
 
-    useEffect(() => {
-        fetch(`http://localhost:8080/issue/${issueId}`)
-            .then(res => res.json())
-            .then(data => setSelectedIssue(data))
-            .catch(error => console.error('Error fetching project:', error));
-    }, []);
+  if (!selectedIssue) {
+    return <p>Laddar...</p>;
+  }
 
-   
-    if (!selectedIssue) {
-        return <p>Laddar...</p>;
-    }
-
-    return (
-        <>
-            <div key={selectedIssue.issueId}>
-                <h2>{selectedIssue.issueName}</h2>
-                <p>{selectedIssue.issueDescription}</p>
-                <p>{"Estimerad tid av medlemmar: " + selectedIssue.estimatedTime}</p>
-                <p>{"Överenskommen tid för issue: " + selectedIssue.agreedTime + "h"}</p>
-                <p>{"Tid spenderad för issue: " + selectedIssue.actualTimeSpent + "h"}</p>
-                <p>{"Skapat av: " + selectedIssue.issueCreatedByUser.username + "/" + selectedIssue.issueDate.toString()}</p>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div key={selectedIssue.issueId}>
+        <h2>{selectedIssue.issueName}</h2>
+        <p>{selectedIssue.issueDescription}</p>
+        <p>{"Estimerad tid av medlemmar: " + selectedIssue.estimatedTime}</p>
+        <p>
+          {"Överenskommen tid för issue: " + selectedIssue.agreedTime + "h"}
+        </p>
+        <p>
+          {"Tid spenderad för issue: " + selectedIssue.actualTimeSpent + "h"}
+        </p>
+        <p>
+          {"Skapat av: " +
+            selectedIssue.issueCreatedByUser.username +
+            "/" +
+            selectedIssue.issueDate.toString()}
+        </p>
+        <IssueIsDone issueId={selectedIssue?.issueId || ""} />
+      </div>
+    </>
+  );
 }
 
 export default SelectedIssue;
