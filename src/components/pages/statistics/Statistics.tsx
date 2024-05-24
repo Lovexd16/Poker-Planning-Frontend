4ec42b1ea0c119c29e55d5618ee4bf646e4b6ea6
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-05-24 17:04:52 +0200

    Nu listas inaktiva projekt på statistics

--- src/components/pages/statistics/Statistics.tsx
+++ src/components/pages/statistics/Statistics.tsx
@@ -1,5 +1,46 @@
+import { useState, useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+
+import ProjectInterface from "../../interface/ProjectInterface";
+
 function Statistics() {
-  return <div>Statistics</div>;
+  const [inactiveProjects, setInactiveProjects] = useState<ProjectInterface[]>(
+    []
+  );
+
+  useEffect(() => {
+    const token = localStorage.getItem("token") || "";
+    const decodedToken = jwtDecode(token);
+    const loggedInUser = decodedToken.sub;
+
+    fetch(`http://localhost:8080/inactiveprojects/${loggedInUser}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setInactiveProjects(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching inactive projects:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Inactive Projects</h2>
+      {inactiveProjects.length > 0 ? (
+        <ul>
+          {inactiveProjects.map((project: ProjectInterface) => (
+            <li key={project.projectId}>{project.projectName}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>You have no inactive projects.</p>
+      )}
+    </div>
+  );
 }
 
 export default Statistics;
